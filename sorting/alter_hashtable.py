c06b3ba773220a0f2aa7169d61022eb89ac2c031
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-02-16 22:56:42 -0500

    Update alter_hashtable.py

--- sorting/alter_hashtable.py
+++ sorting/alter_hashtable.py
@@ -1,8 +1,10 @@
 class hashtable():
+    
     def __init__(self):
         self.size = 1000
         self.table = [None] * self.size
 
+        
     def add(self,item):
         hashcode = self.hash(item)
         if hashcode >= self.size:
