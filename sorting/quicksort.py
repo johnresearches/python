02f71f4661ccad93513c2a4d4e5a356ad0eda81e
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-02-28 21:33:45 -0500

    Update quicksort.py

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -23,11 +23,10 @@ def pivot(array, start, end):
 def quicksort (array, start, end):
     if start >= end:
         return 
-        
     p = pivot(array, start, end)
     quicksort(array,start,p-1)
     quicksort(array,p+1, end)
     
-array = [6,3,3,2,9,2]
+array = [6,3,1,2,9,2]
 quicksort(array, 0, len(array)-1)
 print(array)
