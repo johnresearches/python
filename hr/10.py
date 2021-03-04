10989a71fac3c815ae048c49b8973572ef21bbcf
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-03-03 23:51:45 -0500

    Update 10.py

--- hr/10.py
+++ hr/10.py
@@ -1,7 +1,11 @@
 if __name__ == '__main__':
     n = int(raw_input())
     e = raw_input().split()
+    
     arr = map(int, e)
+    
     lis = set(arr)
+    
     sortedArr = sorted(lis)
+    
     print sortedArr[-2]
