6b1dd5b6787bf82cec4177991f7922f9daa567c1
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-08-24 21:03:56 -0400

    Update mergesort.py

--- sorting/mergesort.py
+++ sorting/mergesort.py
@@ -1,8 +1,11 @@
 def mergesort(alist):
     if len(alist)>1:
         mid = len(alist)//2
+        
         lefthalf = alist[:mid]
+        
         righthalf=alist[mid:]
+        
         mergesort(lefthalf)
         
         mergesort(righthalf)
