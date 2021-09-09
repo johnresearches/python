387d1d86366257708a4907d3020cd9500eb1a4f0
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-09-08 21:26:22 -0400

    Update 3.py

--- hr/3.py
+++ hr/3.py
@@ -4,8 +4,12 @@ if __name__ == '__main__':
     b = int(raw_input())
     
     addition = a+b
+    
     substract = a-b
+    
     product = a*b
+    
     print addition
+    
     print substract
     print product
